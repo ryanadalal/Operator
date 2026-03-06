bf95797c6696e6dc6d9b2c90b1830f7f87ab510a
ryanadalal <ryanadalal@gmail.com>
2026-03-06 18:21:42 -0500

    higher half kernel

--- kernel/arch/i386/boot.s
+++ kernel/arch/i386/boot.s
@@ -6,7 +6,7 @@
 .set CHECKSUM, -(MAGIC + FLAGS) # checksum of above, to prove we are multiboot
 
 # Declare a header as in the Multiboot Standard.
-.section .multiboot
+.section .multiboot, "a"
 .align 4
 .long MAGIC
 .long FLAGS
@@ -19,22 +19,84 @@ stack_bottom:
 .skip 16384 # 16 KiB
 stack_top:
 
+.align 4096
+boot_page_directory:
+	.skip 4096
+boot_page_table1:
+	.skip 4096
+
 # The kernel entry point.
-.section .text
+.section .multiboot.text, "ax"
 .global _start
 .type _start, @function
 _start:
+  # edi points to boot_page_table1
+	movl $(boot_page_table1 - 0xC0000000), %edi
+	# virtual address to map starting at 0
+	movl $0, %esi
+	# the number of pages to map (1023 since 1024 is for VGA text memory)
+	movl $1023, %ecx
+
+# 1 and 2 are used to map the kernel in both the lower half and higher half of the virtual address space.
+1:
+	# map the kernel into the page table
+	cmpl $_kernel_start, %esi
+	jl 2f
+	cmpl $(_kernel_end - 0xC0000000), %esi
+	jge 3f
+
+	movl %esi, %edx
+  # set the present and writable flags to true
+	orl $0x003, %edx
+	movl %edx, (%edi)
+2:
+	# each page is 4096 bytes, so to get next addr add 4096 to esi
+	addl $4096, %esi
+	# each entry is 4 bytes
+	addl $4, %edi
+	# loop until all pages are mapped
+	loop 1b
+3:
+  # maps the vga text buffer and sets present and writable flags (0x003)
+  movl $(0x000B8000 | 0x003), boot_page_table1 - 0xC0000000 + 1023 * 4
+
+	# page directory has 1024 entries, each pointing to a page table
+  # entry 0 maps to boot_page_table1, and entry 768 (0x300) also maps to boot_page_table1 to create the higher half mapping.
+  # the first is identity mapping, and the second is the higher half mapping.
+
+	# Map the page table to both virtual addresses 0x00000000 and 0xC0000000.
+	movl $(boot_page_table1 - 0xC0000000 + 0x003), boot_page_directory - 0xC0000000 + 0
+	movl $(boot_page_table1 - 0xC0000000 + 0x003), boot_page_directory - 0xC0000000 + 768 * 4
+
+	# Set cr3 to the address of the boot_page_directory.
+	movl $(boot_page_directory - 0xC0000000), %ecx
+	movl %ecx, %cr3
+
+	# Enable paging and the write-protect bit.
+	movl %cr0, %ecx
+  # overwrite cr0s paging enabled bit to 1
+  # and set the write-protect bit to 1 as well (0x80010000) to avoid kernel overwriting read-only pages in user space.
+	orl $0x80010000, %ecx
+	movl %ecx, %cr0
+
+	# Jump to higher half with an absolute jump. 
+	lea 4f, %ecx
+	jmp *%ecx
+
+.section .text
+
+4:
 	cli
 
+  # Unmap the identity mapping as it is now unnecessary. 
+	movl $0, boot_page_directory + 0
+
+  # Reload crc3 to force a TLB flush so the changes to take effect.
+	movl %cr3, %ecx
+	movl %ecx, %cr3
+
+  # set up the stack
 	movl $stack_top, %esp
-
-	# Call the global constructors.
-	# call _init
-
-	#
-	# THIS WAS CAUSING ISSUES
-	# CALL INIT IT BROKEN FOR CPP
-	#
 
 	# Transfer control to the main kernel.
 	call kernel_main
@@ -43,4 +105,3 @@ _start:
 	cli
 1:	hlt
 	jmp 1b
-.size _start, . - _start
--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -9,7 +9,7 @@
 
 static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 25;
-static uint16_t* const VGA_MEMORY = (uint16_t*) 0xB8000;
+static uint16_t* const VGA_MEMORY = (uint16_t*) 0xC03FF000;
 
 static size_t terminal_row;
 static size_t terminal_column;
@@ -57,23 +57,22 @@ void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
 * @param line the line to be scrolled up
 */
 void terminal_scroll(int line) {
-	char c;
-    int loop;
-	for(loop = line * (VGA_WIDTH * 2) + 0xB8000; loop < VGA_WIDTH * 2; loop++) {
-		c = *(char *)(loop);
-		*(char *)(loop - (VGA_WIDTH * 2)) = c;
-	}
+    uint16_t* vga = VGA_MEMORY;
+    int start = line * VGA_WIDTH;
+
+    for (size_t i = start; i < VGA_WIDTH * VGA_HEIGHT; i++) {
+        vga[i - VGA_WIDTH] = vga[i];
+    }
 }
 
 /*
 * clear the last line of the terminal
 */
 void terminal_delete_last_line() {
-	int x, *ptr;
+	uint16_t* vga = VGA_MEMORY;
 
-	for(x = 0; x < VGA_WIDTH * 2; x++) {
-		ptr = (int *)(0xB8000 + (VGA_WIDTH * 2) * (VGA_HEIGHT - 1) + x);
-		*ptr = 0;
+	for(size_t x = 0; x < VGA_WIDTH; x++) {
+		vga[(VGA_HEIGHT - 1) * VGA_WIDTH + x] = 0;
 	}
 }
 
